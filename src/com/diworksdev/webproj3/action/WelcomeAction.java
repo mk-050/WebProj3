58ff4d73d11356caa48858a2176d047e5c29c7cc
mk-050 <yoshikawa.0505@outlook.jp>
2024-09-08 14:29:59 +0900

    演習3、22ページまで

---
+++ src/com/diworksdev/webproj3/action/WelcomeAction.java
@@ -0,0 +1,9 @@
+package com.diworksdev.webproj3.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class WelcomeAction extends ActionSupport {
+	public String excecute() {
+		return SUCCESS;
+	}
+}
